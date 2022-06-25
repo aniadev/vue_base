b5ab6426de3c0a0de1a1d2087b39f032b28300fc
haipc_dev <phamcong.hai.90@gmail.com>
2022-06-25 18:30:27 +0700

    create base project for vuejs

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <ul class="data">
+      <li class="data-item" v-for="item in data" :key="item.id">
+        {{ item.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Prop, Vue} from "vue-property-decorator"
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() public msg!: string
+  @Prop() public data!: Array<Record<string, any>>
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" :data="data" />
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue, Prop} from "vue-property-decorator"
+import HelloWorld from "@/components/HelloWorld.vue" // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class HomeView extends Vue {
+  data: Array<Record<string, any>> = [
+    {
+      id: 1,
+      title: "VueJS",
+    },
+    {
+      id: 2,
+      title: "AngularJS",
+    },
+  ]
+}
+</script>
